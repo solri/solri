f794904a661e7db8f573dba1b866b92b674e57eb
Wei Tang <hi@that.world>
2019-07-13 21:13:06 +0200

    Basic block definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use primitive_types::H256;
+
+pub type AccountId = u64;
+pub type Difficulty = u128;
+pub type Balance = u128;
+
+pub struct Block {
+    parent_id: Option<H256>,
+    state_root: H256,
+    coinbase: AccountId,
+    difficulty: Difficulty,
+    nonce: H256,
+}
+
 fn main() {
     println!("Hello, world!");
 }
