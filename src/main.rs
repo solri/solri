df4d996f3eec40b78fe1fdc6496335e91e55187d
Wei Tang <hi@that.world>
2019-07-13 22:04:24 +0200

    All basic transaction and state types

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use primitive_types::H256;
+use primitive_types::{H256, H512};
 
 pub type AccountId = u64;
 pub type Difficulty = u128;
 pub type Balance = u128;
+pub type Nonce = u64;
+pub type Public = H256;
+pub type Signature = H512;
+
+pub enum TransferId {
+    Existing(AccountId),
+    New(Public),
+}
 
 pub struct Block {
-    parent_id: Option<H256>,
-    state_root: H256,
-    coinbase: AccountId,
-    difficulty: Difficulty,
-    nonce: H256,
+    pub unsealed: UnsealedBlock,
+    pub proof: H256,
+}
+
+pub struct UnsealedBlock {
+    pub parent_id: Option<H256>,
+    pub state_root: H256,
+    pub coinbase: TransferId,
+    pub transactions: Vec<Transaction>,
+}
+
+pub struct Account {
+    pub balance: Balance,
+    pub nonce: Nonce,
+    pub public: Public,
+}
+
+pub struct Transaction {
+    pub unsealed: UnsealedTransaction,
+    pub signature: Signature,
+}
+
+pub struct UnsealedTransaction {
+    pub from: AccountId,
+    pub to: TransferId,
+    pub amount: Balance,
 }
 
 fn main() {
