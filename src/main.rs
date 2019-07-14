6076fb7dee5c5d4b243234dd3301fa38a388d675
Wei Tang <hi@that.world>
2019-07-14 15:46:07 +0200

    Use new enum derive from bm-le

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use primitive_types::{H256, H512};
-use bm::Leak;
 use bm_le::{FromTree, IntoTree, tree_root};
 use blockchain::traits::{Block as BlockT};
 use parity_codec::{Encode, Decode};
@@ -13,59 +12,15 @@ pub type Public = H256;
 pub type Signature = H512;
 pub type Hash = Sha3_256;
 
-#[derive(Clone, Eq, PartialEq, Debug, Encode, Decode)]
+#[derive(Clone, Eq, PartialEq, Debug, FromTree, IntoTree, Encode, Decode)]
 pub enum TransferId {
     Existing(AccountId),
     New(Public),
 }
 
-impl<DB> FromTree<DB> for TransferId where
-    DB: bm_le::Backend<Intermediate=bm_le::Intermediate, End=bm_le::End>
-{
-    fn from_tree(
-        root: &bm_le::ValueOf<DB>,
-        db: &DB
-    ) -> Result<Self, bm_le::Error<DB::Error>> {
-        match root {
-            bm_le::Value::End(_) => {
-                Ok(TransferId::Existing(u64::from_tree(root, db)?))
-            },
-            bm_le::Value::Intermediate(_) => {
-                let raw = bm::DanglingRaw::from_leaked(root.clone());
-                Ok(TransferId::New(
-                    H256::from_tree(&raw.get(db, bm::Index::root().left())?.ok_or(
-                        bm_le::Error::CorruptedDatabase
-                    )?, db)?
-                ))
-            },
-        }
-    }
-}
-
-impl<DB> IntoTree<DB> for TransferId where
-    DB: bm_le::Backend<Intermediate=bm_le::Intermediate, End=bm_le::End>
-{
-    fn into_tree(
-        &self,
-        db: &mut DB
-    ) -> Result<bm_le::ValueOf<DB>, bm_le::Error<DB::Error>> {
-        match self {
-            TransferId::Existing(existing) => {
-                existing.into_tree(db)
-            },
-            TransferId::New(new) => {
-                let mut raw = bm::DanglingRaw::default();
-                let left = new.into_tree(db)?;
-                raw.set(db, bm::Index::root().left(), left)?;
-                Ok(raw.metadata())
-            },
-        }
-    }
-}
-
 #[derive(Clone, Eq, PartialEq, Debug, FromTree, IntoTree, Encode, Decode)]
 pub struct UnsealedBlock {
-    pub parent_id: H256,
+    pub parent_id: Option<H256>,
     pub state_root: H256,
     pub coinbase: TransferId,
     pub transactions: Vec<Transaction>,
@@ -81,11 +36,7 @@ impl BlockT for Block {
     type Identifier = H256;
 
     fn parent_id(&self) -> Option<H256> {
-        if self.unsealed.parent_id == H256::default() {
-            None
-        } else {
-            Some(self.unsealed.parent_id.clone())
-        }
+        self.unsealed.parent_id
     }
 
     fn id(&self) -> H256 {
