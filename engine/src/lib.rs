c4629b268b0e18a666819bd76a4d736db4be146f
Wei Tang <hi@that.world>
2019-08-07 22:10:29 +0200

    Error handling for block builder

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -26,6 +26,7 @@ pub enum Error {
 	InvalidFunctionSignature,
 	InvalidMetadata,
 	ExecutionFailed,
+	OutdatedRuntime,
 }
 
 impl std::fmt::Display for Error {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use blockchain::backend::{SharedMemoryBackend, ChainQuery, ImportLock};
-use blockchain::import::ImportAction;
-use blockchain::{Block as BlockT, SimpleBuilderExecutor, AsExternalities};
+use blockchain::backend::{
+	SharedMemoryBackend, ChainQuery, ImportLock, Store,
+	Operation, SharedCommittable
+};
+use blockchain::import::{Error as ImportError, ImportAction};
+use blockchain::{Block as BlockT, SimpleBuilderExecutor, AsExternalities,};
 use blockchain_network_simple::{BestDepthImporter, BestDepthStatusProducer};
 use std::thread;
 use std::collections::HashMap;
@@ -116,37 +119,51 @@ fn libp2p_sync(port: &str, author: bool) {
 
 fn builder_thread(backend_build: SharedMemoryBackend<engine::GenericBlock, (), State>, lock: ImportLock) {
     loop {
-		let head = backend_build.head();
-		let runtime_executor = runtime::Executor;
-		let engine_executor = engine::Executor;
-		println!("Building on top of {:?}", head);
-
-		// Build a block.
-		let parent_block = runtime::Block::decode(
-			&mut &backend_build.block_at(&head).unwrap().data[..]
-		).unwrap();
-		let pending_state = backend_build.state_at(&head).unwrap();
-		assert_eq!(&pending_state.code()[..], runtime::WASM_BINARY);
-
-		let mut unsealed_block = runtime_executor.initialize_block(
-			&parent_block, ().as_externalities(), 1234
-		).unwrap();
-
-		runtime_executor.apply_extrinsic(
-            &mut unsealed_block, runtime::Extrinsic::Add(1), ().as_externalities()
-        ).unwrap();
-
-		runtime_executor.finalize_block(
-			&mut unsealed_block, ().as_externalities(),
-		).unwrap();
-
-		let block = unsealed_block.seal();
-
-		// Import the built block.
-		let mut build_importer = ImportAction::new(&engine_executor, &backend_build, lock.lock());
-		let new_block_hash = block.id()[..].to_vec();
-		build_importer.import_block(block.clone().into()).unwrap();
-		build_importer.set_head(new_block_hash);
-		build_importer.commit().unwrap();
+		build_one(&backend_build, &lock).unwrap();
     }
+}
+
+fn build_one<Ba>(backend_build: &Ba, lock: &ImportLock) -> Result<(), blockchain::import::Error> where
+	Ba: Store<Block=engine::GenericBlock, State=State, Auxiliary=()> + ChainQuery + ?Sized,
+	Ba: SharedCommittable<Operation=Operation<engine::GenericBlock, State, ()>>,
+	blockchain::import::Error: From<Ba::Error>,
+{
+	let head = backend_build.head();
+	let runtime_executor = runtime::Executor;
+	let engine_executor = engine::Executor;
+	println!("Building on top of {:?}", head);
+
+	// Build a block.
+	let parent_block = runtime::Block::decode(
+		&mut &backend_build.block_at(&head).unwrap().data[..]
+	).ok_or(ImportError::Custom(Box::new(engine::Error::OutdatedRuntime)))?;
+	let pending_state = backend_build.state_at(&head).unwrap();
+	if &pending_state.code()[..] != runtime::WASM_BINARY {
+		return Err(ImportError::Custom(Box::new(engine::Error::OutdatedRuntime)))
+	}
+
+	let mut unsealed_block = runtime_executor.initialize_block(
+		&parent_block, ().as_externalities(), 1234
+	).map_err(|e| ImportError::Custom(Box::new(e)))?;
+
+	runtime_executor.apply_extrinsic(
+        &mut unsealed_block, runtime::Extrinsic::Add(1), ().as_externalities()
+    ).map_err(|e| ImportError::Custom(Box::new(e)))?;
+
+	runtime_executor.finalize_block(
+		&mut unsealed_block, ().as_externalities(),
+	).map_err(|e| ImportError::Custom(Box::new(e)))?;
+
+	let block = unsealed_block.seal();
+
+	// Import the built block.
+	let mut build_importer = ImportAction::<engine::Executor, Ba>::new(
+		&engine_executor, backend_build, lock.lock()
+	);
+	let new_block_hash = block.id()[..].to_vec();
+	build_importer.import_block(block.clone().into())?;
+	build_importer.set_head(new_block_hash);
+	build_importer.commit()?;
+
+	Ok(())
 }
