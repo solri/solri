2489be7b1d84feeb1fe204c8574142be7e7b1cb7
Wei Tang <wei@that.world>
2020-07-25 05:39:17 +0200

    Add memory revision db

--- revdb/src/lib.rs
+++ revdb/src/lib.rs
@@ -1,3 +1,12 @@
+extern crate alloc;
+
+mod memory;
+
+pub use crate::memory::{MemoryRevDB, MemoryRevDBError};
+
+/// Revision type.
+pub type Revision = u64;
+
 /// Revision database.
 pub trait RevDB {
 	/// Key type.
@@ -8,15 +17,15 @@ pub trait RevDB {
     type Error;
 
     /// Current revision of the database.
-    fn revision(&self) -> u64;
+    fn revision(&self) -> Revision;
     /// Revert the database back to an earlier revision.
     fn revert_to(&mut self, revision: u64) -> Result<(), Self::Error>;
     /// Get value from the database, of a revision.
-    fn get(&self, revision: u64, key: &[u8]) -> Result<Vec<u8>, Self::Error>;
+    fn get(&self, revision: u64, key: &Self::Key) -> Result<Self::Value, Self::Error>;
     /// Commit values into the database to form a new revision,
     /// returns the new revision number.
-    fn commit<'a, 'b>(
+    fn commit(
         &mut self,
-        values: impl Iterator<Item=(&'a [u8], &'b [u8])>
-    ) -> Result<u64, Self::Error>;
+        values: impl Iterator<Item=(Self::Key, Self::Value)>
+    ) -> Result<Revision, Self::Error>;
 }
---
+++ revdb/src/memory.rs
@@ -0,0 +1,78 @@
+use alloc::collections::btree_map::BTreeMap;
+use crate::{RevDB, Revision};
+
+/// Memory revision DB error.
+pub enum MemoryRevDBError {
+	/// Revert target out of range.
+	InvalidRevertTarget,
+}
+
+/// In-memory revision database.
+pub struct MemoryRevDB<K, V> {
+	db: BTreeMap<K, BTreeMap<Revision, Option<V>>>,
+	journal: BTreeMap<Revision, Vec<K>>,
+	revision: Revision,
+}
+
+impl<K: Ord + Clone, V: Clone> RevDB for MemoryRevDB<K, V> {
+	type Key = K;
+	type Value = Option<V>;
+	type Error = MemoryRevDBError;
+
+	fn revision(&self) -> Revision {
+		self.revision
+	}
+
+    fn revert_to(&mut self, target: u64) -> Result<(), Self::Error> {
+		if target > self.revision {
+			return Err(MemoryRevDBError::InvalidRevertTarget)
+		}
+
+		let mut current = self.revision;
+		while current > target {
+			let keys = self.journal.remove(&current).unwrap_or_default();
+			for key in keys {
+				let to_remove = self.db.get_mut(&key).map(|rvalues| {
+					rvalues.remove(&current);
+					rvalues.len() == 0
+				}).unwrap_or(false);
+				if to_remove {
+					self.db.remove(&key);
+				}
+			}
+			current -= 1;
+		}
+
+		Ok(())
+	}
+
+    fn get(&self, target: u64, key: &Self::Key) -> Result<Self::Value, Self::Error> {
+		if let Some(rvalues) = self.db.get(key) {
+			for (revision, value) in rvalues.iter().rev() {
+				if *revision <= target {
+					return Ok(value.clone())
+				}
+			}
+		}
+
+		Ok(None)
+	}
+
+    fn commit(
+        &mut self,
+        values: impl Iterator<Item=(Self::Key, Self::Value)>
+    ) -> Result<Revision, Self::Error> {
+		let new = self.revision + 1;
+
+		let mut keys = Vec::new();
+		for (key, value) in values {
+			self.db.entry(key.clone()).or_default().insert(new, value);
+			keys.push(key);
+		}
+
+		self.journal.insert(new, keys);
+		self.revision = new;
+
+		Ok(new)
+	}
+}
